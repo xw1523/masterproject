7be2dbea4e7fcc977d3c80ab34a88373e45b9970
XINMAN <zczlanp@gmail.com>
2024-03-12 16:27:52 +0000

    adding NN21

--- NN21.py
+++ NN21.py
@@ -1,98 +1,126 @@
-#import sys
-#sys.path.append('/vols/cms/xw1523/masterproject/prepare_dataframes.py')
-#from prepare_dataframes import process_and_save_combined_chunks
-
-print('the very start')
-
+import uproot
+import numpy as np
 import pandas as pd
-print('a')
+import tensorflow as tf
 import keras
+import json
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, roc_curve, roc_auc_score
+#import matplotlib as mpl
+#mpl.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import numpy as np
+from keras.callbacks import EarlyStopping
+#from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from keras.callbacks import History 
 
 
-def load_data_chunk(chunk_path):
-    print(0)
-    return pd.read_pickle(chunk_path)
+final_chunk=['/vols/cms/xw1523/masterproject/final_chunks/final_chunk_0.pkl',
+             '/vols/cms/xw1523/masterproject/final_chunks/final_chunk_1.pkl',
+             '/vols/cms/xw1523/masterproject/final_chunks/final_chunk_2.pkl',
+             '/vols/cms/xw1523/masterproject/final_chunks/final_chunk_3.pkl',
+             '/vols/cms/xw1523/masterproject/final_chunks/final_chunk_4.pkl']
 
-def prepare_data(mc_chunk, combined_chunk):
-    # 分配标签
-    mc_chunk['label'] = 0
-    combined_chunk['label'] = 1
-    
-    # 合并数据
-    data = pd.concat([mc_chunk, combined_chunk], ignore_index=True)
-    print(data.head())
-    X = data[['Z_mass', 'Z_pt']]
-    y = data['label']
-    
-    return X, y
-    print(1)
-    
-def build_model(input_shape):
-    model = Sequential([
-        Dense(2,activation='relu', input_dim=2),
-        Dense(8, activation='relu'),
-        Dense(1, activation='sigmoid')
-    ])
+
+# define a simple NN
+def baseline_model(input_dimension):
+    # create model
+    model = Sequential()
+    model.add(Dense(input_dimension, input_dim=input_dimension, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(input_dimension*2, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, activation="sigmoid"))
     opt = keras.optimizers.Adam(learning_rate=0.1)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=opt, loss='binary_crossentropy')
     return model
-    print(2)
-    
-def update_weights(mc_data, predictions_prob):
-    # 计算新重
-    predictions_prob=np.minimum(0.9999,predictions_prob)
-    new_weights = predictions_prob / (1 - predictions_prob)
-    # 更新MC数据的权重
-    
-    mc_data['new_weight'] = new_weights.flatten() * mc_data['wt']
-    return mc_data
-    print(3)
-    
-def plot_weighted_distribution(data, weight_col, feature, bins, label, range=None):
-    plt.hist(data[feature], weights=data[weight_col], bins=bins, alpha=0.5, label=label, range=range)
-    plt.xlabel(feature)
-    plt.ylabel('Weighted Events')
-    plt.title(f'Weighted {feature} Distribution')
-    plt.legend()
 
-n_chunks = 1
-for i in range(n_chunks):
-    print('start')
-    mc_chunk_path = f'MC_chunks/MC_chunk_{i}.pkl'
-    combined_chunk_path = f'combined_chunks/combined_chunk_{i}.pkl'
+
+
+def prepare_data(chunk_path):
+    merged_dff=pd.read_pickle(chunk_path)
+    print("Loaded DataFrame columns:", merged_dff.columns)  # 打印列名
+    X = merged_dff[['Z_mass', 'Z_pt']]
+    y = merged_dff['label']
+    weights = merged_dff['wt']
     
-    mc_chunk = load_data_chunk(mc_chunk_path)
-    combined_chunk = load_data_chunk(combined_chunk_path)
-    
-    X, y = prepare_data(mc_chunk, combined_chunk)
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=42)
-    
-    model = build_model(input_shape=(X.shape[1],))
-    model.fit(X_train, y_train, epochs=1, batch_size=32, verbose=0)  # 设置verbose=0以减少日志输出
-    
-    print(5)
-    
-    # 使用模型预测整个MC数据集
-    predictions_prob = model.predict(mc_chunk[['Z_mass', 'Z_pt']])
-    
-    # 更新权重
-    mc_chunk_updated = update_weights(mc_chunk, predictions_prob)
-    
-    print(6)
-    
-    # 绘制加权的Z_mass和Z_pt分布图
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plot_weighted_distribution(mc_chunk_updated, 'new_weight', 'Z_mass', bins=50, label='MC Weighted', range=[50, 200])
 
     
-    plt.subplot(1, 2, 2)
-    plot_weighted_distribution(mc_chunk_updated, 'new_weight', 'Z_pt', bins=50, label='MC Weighted', range=(0, 200))
-    plt.legend()
-    plt.savefig("NN  Distribution.png")
+    scaler_x = MinMaxScaler()
+    X=scaler_x.fit_transform(X)
+    
+    # define early stopping
+    early_stop = EarlyStopping(monitor='val_loss',patience=10)
+    
+    X_train,X_test, y_train, y_test, weights_train, weights_test   = train_test_split(
+    X,
+    y,
+    weights,
+    test_size=0.95,
+    random_state=46,
+    stratify=y.values,
+)
+    
+    history = History()
+
+    model = baseline_model(X_train.shape[1])
+
+
+    #model.fit(
+               # X_train, y_train,
+                #sample_weight=weights_train,
+               # batch_size=32,
+              #  epochs=10,
+               # callbacks=[history,early_stop],
+                #validation_data=(X_test, y_test))
+    
+    # 将训练和测试数据转换为tf.data.Dataset对象
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train, weights_train)).batch(32)
+    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test, weights_test)).batch(32)
+    
+    # 使用包含权重的Dataset进行训练和验证
+    model.fit(
+        train_dataset,
+        epochs=10,
+        callbacks=[history, early_stop],
+        validation_data=test_dataset
+    )
+    
+    model_save_path=chunk_path.replace('.pkl', '_model11.h5')
+    model.save(model_save_path)   
+    
+    
+     # 保存训练历史
+    history_path = chunk_path.replace('.pkl', '_history.json')
+    with open(history_path, 'w') as file:
+        json.dump(history.history, file)
+
+
+    # 使用模型进行预测
+    y_pred_probs = model.predict(X_test).ravel()
+    
+    # 计算ROC曲线参数和AUC
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_probs)
+    auc_score = roc_auc_score(y_test, y_pred_probs)
+    
+    # 保存ROC数据
+    roc_data = {
+        'fpr': fpr.tolist(),
+        'tpr': tpr.tolist(),
+        'thresholds': thresholds.tolist(),
+        'auc_score': auc_score
+    }
+    roc_data_path = chunk_path.replace('.pkl', '_roc_data.json')
+    with open(roc_data_path, 'w') as file:
+        json.dump(roc_data, file)
+  
+    
+    print(f"Traning completed for {chunk_path}. Model,history,roc saved. ") 
+    
+    
+for chunk_path in final_chunk:
+   prepare_data(chunk_path) 
