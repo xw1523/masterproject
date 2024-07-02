475e114e8783023b480a544afef4e838e179f70b
Ya <liya010224@163.com>
2024-07-02 11:25:51 +0100

    dataloader used in training

--- code/dataloader.py
+++ code/dataloader.py
@@ -4,7 +4,7 @@ def get_generator(file_paths):
     def generator():
         for file_path in file_paths:
             df = pd.read_pickle(file_path)
-            X = df[['Z_mass', 'Z_pt']].values
+            X = df[['Z_mass', 'Z_pt', 'n_jets', 'n_deepbjets', 'mjj', 'jdeta', 'jdphi', 'dijetpt', 'jpt_1', 'jpt_2', 'jpt_3','U1','U2','met','met_phi']].values
             y = df['label'].values
             weights = df['wt'].values
 
